12d9b69fd54ac41a2120e6ac366ac30a6b62b148
Sun Haoyu <99975320+AllenSun6@users.noreply.github.com>
2023-11-10 16:41:24 +0800

    Create Course_data.py

---
+++ Course_data.py
@@ -0,0 +1,3 @@
+course1 = Course(course_id="030815", course_name="COMP1117", teacher=Teacher.objects.get(name="Dr. Loretta Choi"), description="Computer Programming")
+course2 = Course(course_id="030811", course_name="COMP2119", teacher=Teacher.objects.get(name="Dr. H.T.H. Chan"), description="Introduction to data structures and algorithms")
+course3 = Course(course_id="030812", course_name="COMP2120", teacher=Teacher.objects.get(name="Dr. Qi Zhao"), description="Computer organization")
