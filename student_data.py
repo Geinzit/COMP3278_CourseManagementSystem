6ab9b64393cffd74a8776584fc38f1eb00dd929c
Sun Haoyu <99975320+AllenSun6@users.noreply.github.com>
2023-11-10 17:28:42 +0800

    Create student_data.py

---
+++ student_data.py
@@ -0,0 +1,32 @@
+students_data = [
+    {
+        "name": "3035772432",
+        "usrname": "Zhiheng Lyu",
+        "email": "cogito@connect.hku.hk",
+        "bio": "Zhiheng Lyu, a computer science enthusiast, is known for his passion for artificial intelligence and its applications in solving real-world problems."
+    },
+    {
+        "name": "3035835595",
+        "usrname": "Yujing Wang",
+        "email": "yujingww@connect.hku.hk",
+        "bio": "Yujing Wang, an aspiring software developer, has a keen interest in developing user-friendly applications that make daily tasks more efficient."
+    },
+    {
+        "name": "3035856044",
+        "usrname": "Haoyu Sun",
+        "email": "u3585604@connect.hku.hk",
+        "bio": "Haoyu Sun, with a strong foundation in mathematics, excels in creating algorithms that optimize data processing and analysis."
+    },
+    {
+        "name": "3035844077",
+        "usrname": "Maodian Huang",
+        "email": "hmdian@connect.hku.hk",
+        "bio": "Maodian Huang is passionate about web development, focusing on building responsive and accessible websites for diverse audiences."
+    },
+    {
+        "name": "3035780922",
+        "usrname": "Nanqi Gong",
+        "email": "gongnq@connect.hku.hk",
+        "bio": "Nanqi Gong has a strong interest in network security, dedicating his studies to understanding and mitigating cyber threats."
+    }
+]
