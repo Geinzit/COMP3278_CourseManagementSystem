3e6b14f4938d7bb5d80602ab0a4c07651b2cd5a1
cogito233 <cogito@connect.hku.hk>
2023-11-16 17:54:10 +0800

    add photo capture function

--- manager/urls.py
+++ manager/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path("", views.index, name="index"),
-}
+    path('login/', views.login_page, name='login'),
+]
--- manager/views.py
+++ manager/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import base64
+from PIL import Image
+from io import BytesIO
 
 from .models import Student, Teacher, Course, CourseSchedule, Enrollment
 
@@ -14,3 +17,17 @@ def details(request, course_id):
 
     # update later
     
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        image_data = request.POST.get('photo')
+        format, imgstr = image_data.split(';base64,') 
+        image = Image.open(BytesIO(base64.b64decode(imgstr)))
+
+        # 在这里处理用户名和照片
+        # 例如，保存照片，验证用户名等
+        print(username)
+
+        return HttpResponse(f"Username: {username}, Photo received.")
+
+    return render(request, 'login.html')
