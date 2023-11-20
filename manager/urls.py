8fa15f1b82b0f25ae7cc59171187a1e97aee5c67
Sun <99975320+AllenSun6@users.noreply.github.com>
2023-11-20 22:27:05 +0800

    Update urls.py

--- manager/urls.py
+++ manager/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('login/', views.login_page, name='login'),
     path('course/<str:course_id>/', views.course, name='course_detail'),
-    path('course/<str:course_id>/details', views.course_details, name='course1_information'),
+    path('course/<str:course_id>/detail', views.course_detail, name='course1_information'),
     path('curriculum/', views.curriculum, name='curriculum'),
     path("teacher/<int:teacher_id>/", views.teacher),
     path('add_course/<str:course_id>/', views.add_course, name='add_course'),
